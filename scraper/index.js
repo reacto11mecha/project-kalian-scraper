09129318d34d714e849a674aaa31f57f3f63797b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-11-05 14:36:14 +0700

    feat: more robust (i guess)

--- scraper/index.js
+++ scraper/index.js
@@ -21,12 +21,12 @@ const {
 
   // Branches data fetch
   const branchRequest = await axios.get(
-    "https://api.github.com/repos/sandhikagalih/project-kalian/branches"
+    "https://api.github.com/repos/sandhikagalih/project-kalian/branches",
   );
   const branchesData = await branchRequest.data;
 
   logger.info(
-    "Successfully fetched branches data, fethching all readme contents by branches"
+    "Successfully fetched branches data, fethching all readme contents by branches",
   );
 
   const branches = branchesData.map(({ name }) => name);
@@ -34,7 +34,7 @@ const {
   const readmeContents = await Promise.all(
     branches.map(async (branch) => {
       const request = await axios.get(
-        `https://raw.githubusercontent.com/sandhikagalih/project-kalian/${branch}/README.md`
+        `https://raw.githubusercontent.com/sandhikagalih/project-kalian/${branch}/README.md`,
       );
 
       const rawData = await request.data;
@@ -46,131 +46,157 @@ const {
       const data = formattedData.trim();
 
       return { branch, data };
-    })
+    }),
   );
   // End of branches data fetch
 
   logger.info("Done fetching all of required data, now working with readme...");
 
-  const perSeasonData = readmeContents.map((season) => {
-    logger.info(`Working with ${season.branch} branch...`);
+  const perSeasonData = await Promise.all(
+    readmeContents.map(async (season) => {
+      logger.info(`Working with ${season.branch} branch...`);
 
-    // Sanitize text from unwanted content
-    const text = season.data
-      .replaceAll("<br>", "")
-      .replaceAll("<br/>", "")
-      .replaceAll("<br />", "")
-      .replaceAll("<hr>", "")
-      .replaceAll("####SPONSOR", "")
-      .replaceAll("###SPONSOR", "");
+      // Sanitize text from unwanted content
+      const text = season.data
+        .replaceAll("<br>", "")
+        .replaceAll("<br/>", "")
+        .replaceAll("<br />", "")
+        .replaceAll("<hr>", "")
+        .replaceAll("####SPONSOR", "")
+        .replaceAll("###SPONSOR", "");
 
-    // Split every line
-    const splitted = text.split(/\r?\n/);
+      // Split every line
+      const splitted = text.split(/\r?\n/);
 
-    // Search for every showcase date
-    const validDate = splitted
-      // Map every line that started with ### and the content is the showcase date
-      .map((d, idx) => {
-        if (
-          d.startsWith("###") &&
-          !d.startsWith("###SPONSOR") &&
-          !d.startsWith("####")
-        ) {
-          return { date: d.replace("###", "").trim(), idx };
-        }
-      })
+      // Search for every showcase date
+      const validDate = splitted
+        // Map every line that started with ### and the content is the showcase date
+        .map((d, idx) => {
+          if (
+            d.startsWith("###") &&
+            !d.startsWith("###SPONSOR") &&
+            !d.startsWith("####")
+          ) {
+            return { date: d.replace("###", "").trim(), idx };
+          }
+        })
 
-      // Filter only the valid value
-      .filter((d) => !!d);
+        // Filter only the valid value
+        .filter((d) => !!d);
 
-    // Map every showcase date
-    const dates = validDate.map((dateInfo, idx) => {
-      let temp = [];
+      // Map every showcase date
+      const dates = await Promise.all(
+        validDate.map(async (dateInfo, idx) => {
+          let temp = [];
 
-      // Initial file line to read
-      const inital = dateInfo.idx;
+          // Initial file line to read
+          const inital = dateInfo.idx;
 
-      // Last line of the file to read
-      const maxedValue =
-        idx !== validDate.length - 1
-          ? validDate[idx + 1].idx
-          : splitted.length - 1;
+          // Last line of the file to read
+          const maxedValue =
+            idx !== validDate.length - 1
+              ? validDate[idx + 1].idx
+              : splitted.length - 1;
 
-      // Loop from the range
-      for (let i = inital + 1; i < maxedValue; i++) {
-        temp.push(`${splitted[i]}\n`);
-      }
+          // Loop from the range
+          for (let i = inital + 1; i < maxedValue; i++) {
+            temp.push(`${splitted[i]}\n`);
+          }
 
-      const actualContent = marked.parse(
-        // Merge value to get the original form
-        temp
-          .join("")
-          .trim()
+          // Merge value to get the original form
+          const rawText = temp.join("").trim();
 
-          // Replace [<link>] for consistent href
-          .replaceAll("[", "")
-          .replaceAll("]", "")
-          .trim()
-      );
+          // Entering read line by line mode per project
+          const splittedRawText = rawText.split(/\r?\n/);
 
-      // Using JSDOM for more robust reading
-      const dom = new JSDOM(`<!DOCTYPE html>${actualContent}`);
+          const projectsBase = splittedRawText
+            .map((element, idx) => {
+              const splittedNumber = element.split(".");
 
-      return {
-        date: dateInfo.date,
-        projects: [...dom.window.document.querySelector("ol").children].map(
-          (li, projectIdx) => {
-            // Get anchor (project link) and strong element
-            const linkElement = li.querySelector("a");
-            const usernameElement = li.querySelector("strong");
+              const projectNumber = parseInt(splittedNumber[0]);
+              const link = splittedNumber
+                .slice(1)
+                .join(".")
+                .trim()
+                .replace("[", "")
+                .replace("]", "");
 
-            // Image path
-            const image = `${season.branch
-              .toUpperCase()
-              .replace("MAIN", "SEASON-1")}-${dateInfo.date}-${projectIdx}.png`;
+              if (
+                !isNaN(projectNumber) &&
+                new RegExp("^(http|https)://", "i").test(link)
+              ) {
+                return { projectIdx: projectNumber - 1, link, arrayIdx: idx };
+              }
+            })
+            .filter((v) => !!v);
 
-            // Actual project link
-            const link = linkElement.href;
+          const projects = await Promise.all(
+            projectsBase.map(async (project, idx) => {
+              // Image path
+              const image = `${season.branch
+                .toUpperCase()
+                .replace("MAIN", "SEASON-1")}-${dateInfo.date}-${idx}.png`;
 
-            if (!usernameElement) {
-              linkElement.remove();
+              const usernameDOM = new JSDOM(
+                `<!DOCTYPE html>${marked.parse(
+                  splittedRawText[project.arrayIdx + 1].trim(),
+                )}`,
+              );
+              const usernameElement =
+                usernameDOM.window.document.querySelector("strong");
+
+              const username = usernameElement
+                ? usernameElement.textContent
+                : "";
+
+              let secondTemp = [];
+
+              // Initial project to read
+              const initial = project.arrayIdx + 2;
+
+              // Last line of the project to read
+              const maxedValue =
+                idx !== projectsBase.length - 1
+                  ? projectsBase[idx + 1].arrayIdx
+                  : splittedRawText.length - 1;
+
+              // Loop from the range
+              for (let i = initial + 1; i < maxedValue; i++) {
+                secondTemp.push(`${splittedRawText[i].trim()}\n`);
+              }
+
+              // Merge value to get the original form
+              const projectRawText = secondTemp.join("").trim();
+              const prettifyProject = await prettier.format(projectRawText, {
+                parser: "markdown",
+              });
+
+              const message = marked.parse(prettifyProject).trim();
 
               return {
-                link,
-                message: li.innerHTML.trim(),
-                username: "",
                 image,
+                username,
+                message,
+                link: project.link,
               };
-            }
+            }),
+          );
 
-            linkElement.remove();
-            usernameElement.remove();
+          return {
+            date: dateInfo.date,
+            projects,
+          };
+        }),
+      );
 
-            // Removing junk paragraph after link and username get deleted
-            const junkParagraph = li.querySelector("p");
+      logger.info(`Done working with ${season.branch} branch.`);
 
-            const junks = [...junkParagraph.children];
-
-            if (junks.length === 1) junkParagraph.remove();
-
-            return {
-              link,
-              username: usernameElement.textContent,
-              message: li.innerHTML.trim(),
-              image,
-            };
-          }
-        ),
+      return {
+        season: season.branch.toUpperCase().replace("MAIN", "SEASON-1"),
+        dates,
       };
-    });
-
-    logger.info(`Done working with ${season.branch} branch.`);
-
-    return {
-      season: season.branch.toUpperCase().replace("MAIN", "SEASON-1"),
-      dates,
-    };
-  });
+    }),
+  );
 
   const fetched_at = new Date();
 
@@ -196,8 +222,8 @@ const {
         username: projectItem.username,
         message: projectItem.message,
         image: `${seasonItem.season}-${dateItem.date}-${projectIdx}.png`,
-      }))
-    )
+      })),
+    ),
   );
 
   const baseProjectsData = {
@@ -213,8 +239,8 @@ const {
     JSON.stringify(
       { ...byProjectData, data: byProjectData.data.reverse() },
       null,
-      2
-    )
+      2,
+    ),
   );
 
   logger.info("Done saving files. Screenshooting new links...");
